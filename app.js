f3ae7bad938a52dcd9b12e98ce3a967cd131aab0
Divyanshu Jain <jaindivyanshu.personal@gmail.com>
2024-11-24 12:43:28 +0530

    Basic connection completed

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const createHttpError = require('http-errors')
+const { default: mongoose } = require('mongoose')
+const morgan = require('morgan')
+// const mongoose= require('mongoose')
+require('dotenv').config()
+
+
+const app = express()
+
+
+app.use(morgan('dev'));
+
+
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!')
+});
+
+app.use((req, res, next)=>{
+    next(createHttpError.NotFound())
+});
+
+app.use((error, req, res, next)=>{
+    error.status = error.status || 500
+    res.status(error.status)
+    res.send(error)
+})
+
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('📚 Connected to MongoDB')
+    app.listen(PORT, ()=> console.log(`🚀 on port ${PORT}`))
+}).catch(err=>console.log(err.message));
+
+
+
