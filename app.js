b1b31d0308eabce470f493caab5d5d4961b4b6ed
Divyanshu Jain <jaindivyanshu.personal@gmail.com>
2024-11-24 15:17:41 +0530

    Structur with some css

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const createHttpError = require('http-errors')
 const { default: mongoose } = require('mongoose')
 const morgan = require('morgan')
-// const mongoose= require('mongoose')
+const path = require('path')
 require('dotenv').config()
 
 
@@ -11,11 +11,14 @@ const app = express()
 
 app.use(morgan('dev'));
 
+app.set('view engine', 'ejs');
+// app.use(express.static('pubic'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!')
-});
+app.use('/', require('./routes/index.route'));
+app.use('/auth', require('./routes/auth.route'));
+app.use('/user', require('./routes/user.route'));
 
 app.use((req, res, next)=>{
     next(createHttpError.NotFound())
@@ -24,7 +27,8 @@ app.use((req, res, next)=>{
 app.use((error, req, res, next)=>{
     error.status = error.status || 500
     res.status(error.status)
-    res.send(error)
+    res.render('error_40x', {error})
+    // res.send(error)
 })
 
 const PORT = process.env.PORT || 3000
